d7139495c4e2c13e12a1edffff98270b719ef382
Iniubong Arthur <arthurshur@gmail.com>
2022-03-18 02:59:47 +0000

    added Post to admin

--- django_project/djangoO/blog/views.py
+++ django_project/djangoO/blog/views.py
@@ -1,34 +1,14 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
-
-posts = [
-    {
-        "title": "New Blog Post",
-        "author": "Khaleed",
-        "published_date": "17th March, '22",
-        "content": "This is an incredible content here"
-    },
-    {
-        "title": "Another Blog Post",
-        "author": "Azeez",
-        "published_date": "16th March, '22",
-        "content": "This is a brand new content here!"
-    },
-    {
-        "title": "First Blog Post",
-        "author": "Kareem",
-        "published_date": "15th March, '22",
-        "content": "A blog post with incredible content here"
-    }
-]
 
 def home(request):
     return render(
         request,
         'blog/home.html',
         {
-            "posts": posts
+            "posts": Post.objects.all()
         }
     )
 
