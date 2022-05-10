999613d8b0f8e62ec337f1150855591c14bd4411
Iniubong Arthur <arthurshur@gmail.com>
2022-05-10 00:52:36 +0000

    zip address

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -63,6 +63,7 @@ class Addres(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    zip = models.CharField(max_length=255, default='23401')
 
 class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
