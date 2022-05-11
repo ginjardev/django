2349b045cd680033fbb8f024405370ef5ee6593b
Iniubong Arthur <arthurshur@gmail.com>
2022-05-11 19:06:00 +0000

    at orm

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -38,7 +38,7 @@ class Customer(models.Model):
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
-
+ 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
     PAYMENT_STATUS_COMPLETE = 'C'
--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -79,16 +79,18 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'storefront.wsgi.application'
-
+WSGI_APPLICATION = 'storefront.wsgi.application' 
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'sunshine'
     }
 }
 
