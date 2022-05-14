4903dac8b498b1b603d392423fb35a3d05754342
Iniubong Arthur <arthurshur@gmail.com>
2022-05-14 22:59:58 +0000

    query sets

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,12 +1,18 @@
+import imp
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.db.models import Q, F
+from store.models import Collection, Product, OrderItem
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 def say_hello(request):
+    queryset = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+     
     return render( 
         request,
         'hello.html',
         {
-            "name": "Ini"
+            "name": "Ini",
+            'products': list(queryset)
         }
     )
