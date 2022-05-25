b1e833cf00da29a8a201c4d882523a511a5091d2
Iniubong Arthur <arthurshur@gmail.com>
2022-05-25 07:56:17 +0000

    storefront part2

---
+++ storefront2/store/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProductSerializer
+from .models import Product
+from rest_framework import status
+# Create your views here.
+
+@api_view()
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True) 
+    return Response(serializer.data) 
+
+@api_view()
+def product_detail(request, id):
+    try:
+        product = Product.objects.get(pk=id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
