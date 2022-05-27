b45fcaf977ab7a6021d920c91c1fadc620ae341c
Iniubong Arthur <arthurshur@gmail.com>
2022-05-27 21:54:42 +0000

    serializers

--- storefront2/store/views.py
+++ storefront2/store/views.py
@@ -1,5 +1,7 @@
+from math import prod
+from urllib import request
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import ProductSerializer
