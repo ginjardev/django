bacaa2b7ddd0a4eb25296e35d1d0e51867c72dc6
Iniubong Arthur <arthurshur@gmail.com>
2022-05-29 23:46:31 +0000

    serializers

--- storefront2/store/serializers.py
+++ storefront2/store/serializers.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers 
 from store.models import *
-from  decimal import Decimal
+from  decimal import Decimal 
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-
+    collection = serializers.PrimaryKeyRelatedField(
+        queryset = Collection.objects.all()
+    )
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
