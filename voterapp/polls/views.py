33e02ba8623e50b12265b6a196d8cca3fea366fa
Iniubong Arthur <arthurshur@gmail.com>
2022-06-06 23:05:23 +0000

    django template

--- voterapp/polls/views.py
+++ voterapp/polls/views.py
@@ -1,12 +1,18 @@
+from unittest import loader
 from django.shortcuts import render
 from django.http import HttpResponse
 from polls.models import *
+from django.template import loader
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.all()
     output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
