cd5bf762a13c47d8b399d3dc15c6218baf2eb4d7
Iniubong Arthur <arthurshur@gmail.com>
2022-07-05 05:07:17 +0000

    welcome back

--- storefront2/store/views.py
+++ storefront2/store/views.py
@@ -3,14 +3,48 @@ from urllib import request, response
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpRequest, HttpResponse
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import CollectionSerializer, ProductSerializer
 from .models import *
 from django.db.models import Count
 from rest_framework import status
 
-from store import serializers
+from store import serializers 
 # Create your views here.
+class ProductList(APIView):
+    def get(self, request):
+        products = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(products, many=True, context={'request': request}) 
+        return Response(serializer.data) 
+
+    def post(self, request):
+        serializer = ProductSerializer(data= request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ProductDetail(APIView):
+    def get(self, request, id):
+        product = Product.objects.get(pk=id) 
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        serializer = ProductSerializer(data= request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def delete(self,request, id):
+        product = Product.objects.get(pk=id)
+        if product.orderitems.count() > 0:
+            return Response({'error', 'Product cannot delete because of order item'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 
 @api_view(['GET', 'POST'])
 def product_list(request):
