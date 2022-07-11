e339579030ffa162bbaaef209344039b0b5fd573
Iniubong Arthur <arthurshur@gmail.com>
2022-07-11 11:44:35 +0000

    Mixins

--- storefront2/store/views.py
+++ storefront2/store/views.py
@@ -5,6 +5,7 @@ from django.http import HttpRequest, HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView
 from .serializers import CollectionSerializer, ProductSerializer
 from .models import *
 from django.db.models import Count
@@ -12,17 +13,12 @@ from rest_framework import status
 
 from store import serializers 
 # Create your views here.
-class ProductList(APIView):
-    def get(self, request):
-        products = Product.objects.select_related('collection').all()
-        serializer = ProductSerializer(products, many=True, context={'request': request}) 
-        return Response(serializer.data) 
+class ProductList(ListCreateAPIView): 
+    queryset = Product.objects.select_related('collection').all()
+    serializer_class = ProductSerializer
 
-    def post(self, request):
-        serializer = ProductSerializer(data= request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def get_serializer_context(self):
+        return {'request': self.request}
 
 
 class ProductDetail(APIView):
@@ -45,17 +41,12 @@ class ProductDetail(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class CollectionList(APIView):
-    def get(self, request):
-        queryset = Collection.objects.annotate(products_count=Count('products')).all()
-        serializer = CollectionSerializer(queryset, many=True)
-        return Response(serializer.data)
+class CollectionList(ListCreateAPIView):
+    queryset = Collection.objects.annotate(products_count=Count('products')).all()
+    serializer_class = CollectionSerializer
 
-    def post(self, request):
-        serializer = CollectionSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def get_serializer_context(self):
+        return {'request': self.request}
 
 
 class CollectionDetail(APIView):
@@ -94,6 +85,7 @@ class CollectionDetail(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+# function-based views
 
 @api_view(['GET', 'POST'])
 def product_list(request):
